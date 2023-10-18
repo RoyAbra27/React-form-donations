f4c19abbdb26f745ffff7d81222906fa687b4f1d
Idan <ghsijns@gmail.com>
2023-10-18 21:35:40 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -19,16 +19,35 @@ else
     # Filter objects with .css and .js extensions and delete them
     for object in $OBJECTS; do
         if [[ $object == *.css ]] || [[ $object == *.js ]]; then
-            echo "Deleting $object from $BUCKET_NAME ..."
+            echo "Deleting $object from $BUCKET_NAME..."
             oci os object delete --bucket-name="$BUCKET_NAME" --name="$object" --force
         fi
     done
 
     # Iterate through each file in the directory and upload to bucket
     find "$DIR_TO_UPLOAD" -type f | while read -r FILE; do
-        OBJECT_NAME=$(basename "$FILE")
-        echo "Uploading $FILE to $BUCKET_NAME/$OBJECT_NAME ..."
-        oci os object put --bucket-name="$BUCKET_NAME" --name="$OBJECT_NAME" --file="$FILE" --force
+        # Extract relative path from DIR_TO_UPLOAD
+        RELATIVE_PATH=${FILE#"$DIR_TO_UPLOAD/"}
+
+        # Determine content type based on file extension
+        CONTENT_TYPE=""
+        case "$RELATIVE_PATH" in
+            *.html)  CONTENT_TYPE="text/html";;
+            *.css)   CONTENT_TYPE="text/css";;
+            *.js)    CONTENT_TYPE="application/javascript";;
+            *.png)   CONTENT_TYPE="image/png";;
+            *.svg)   CONTENT_TYPE="image/svg+xml";;
+            # Add other file types if needed
+            *)       CONTENT_TYPE="";;
+        esac
+
+        echo "Uploading $FILE to $BUCKET_NAME/$RELATIVE_PATH with content type $CONTENT_TYPE..."
+
+        if [ -n "$CONTENT_TYPE" ]; then
+            oci os object put --bucket-name="$BUCKET_NAME" --name="$RELATIVE_PATH" --file="$FILE" --force --content-type "$CONTENT_TYPE"
+        else
+            oci os object put --bucket-name="$BUCKET_NAME" --name="$RELATIVE_PATH" --file="$FILE" --force
+        fi
     done
 
     echo "Upload completed!"
